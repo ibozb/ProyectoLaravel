56f7dc6a8fbcdf0302ac6b60145b7b85f71b36bf
Aiub Filali <aiub.tel@gmail.com>
2025-02-07 13:33:44 +0100

    Tercera confirmación

--- TiendaComponentes/routes/web.php
+++ TiendaComponentes/routes/web.php
@@ -2,10 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
 });
 
-Route::get('/greeting', function(){
-    return 'Hola Mundo';
+Route::get('/equipo', function(){
+    return '<h1> Devolución de los equipos </h1>';
+});
+
+Route::get('/equipo/{id}', function($identificador){
+    return '<h1>Equipo </h1>'.$identificador;
+});
+
+Route::get('/equipo/{id}/comentario/{comment}', function($id, $comentario){
+    return 'Equipo '.$id.', Excelente Trabajo '.$comentario;
+});
+
+Route::get('/usuario/{nombre?}', function($nombre = 'Aiub'){
+    return '<h1>Nombre </h1>'.$nombre;
 });
